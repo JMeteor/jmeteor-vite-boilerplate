abc35943ae2c2f6dede457acdf48c4243b9c184a
jmeteor <jerzy.zientek@gmail.com>
2022-05-16 21:51:09 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './styles/css/normalize.css'
+import './styles/scss/main.scss';
+
+
+document.querySelector('#app').innerHTML = `
+  <h1>Welcome to Jerzy Meteor Vite Boilerplate!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a>
+`
---
+++ vite.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+import viteImagemin from 'vite-plugin-imagemin';
+import legacy from '@vitejs/plugin-legacy'
+
+export default{
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+    }
+  },
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ]
+}
